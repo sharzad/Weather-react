56e386a7fbcca25e86fb169f1a4788b57261f02f
sharzad <babaeishahrzad@gmail.com>
2023-03-20 03:16:01 +0330

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import React from "react";
+import Search from "./Search";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>WEATHER APP</h1>
+      <Search />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        netlify link
+      </a>
+      <br></br>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Githublink
+      </a>
+    </div>
+  );
+}
