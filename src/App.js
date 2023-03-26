bd1334017940805a21d72cefcbffe1fc5d0de059
sharzad <babaeishahrzad@gmail.com>
2023-03-27 00:53:58 +0200

    added bootstarp

--- src/App.js
+++ src/App.js
@@ -5,25 +5,30 @@ import Search from "./Search";
 export default function App() {
   return (
     <div className="App">
-      <h1>WEATHER APP</h1>
-      <Search />
-      <a
-        className="App-link"
-        href="https://loquacious-yeot-0cdb50.netlify.app/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        netlify link
-      </a>
-      <br></br>
-      <a
-        className="App-link"
-        href="https://github.com/sharzad/Weather-react"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Githublink
-      </a>
+      <div className="container">
+        <h1>WEATHER APP</h1>
+        <Search />
+        <footer>
+          This project was created by Shahrzad B. and it is open source {""}
+          <a
+            className="App-link"
+            href="https://loquacious-yeot-0cdb50.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            netlify link
+          </a>
+          {" & "}
+          <a
+            className="App-link"
+            href="https://github.com/sharzad/Weather-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Githublink
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
